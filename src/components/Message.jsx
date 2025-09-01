51678a78f3f3d0c66178e384e04adfe0192d7d93
Ajay <smilyboy2207@gmail.com>
2025-09-02 00:14:04 +0530

    Message chnage

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import "./message.css"
+import "./Message.css"
 
 const Message = () => {
   const [selectedContact, setSelectedContact] = useState("anto-maria")
